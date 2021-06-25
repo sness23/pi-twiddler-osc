e0912523501003e904bfd4441eb183c398b9e26c
sness <snessnet@gmail.com>
2021-06-25 14:11:29 -0700

    Added code to do 4 character mappings

--- pi-twiddler-osc.py
+++ pi-twiddler-osc.py
@@ -1,16 +1,37 @@
 import tkinter as tk
 import sys
 from pythonosc import udp_client
+import syslog
+outStr = ""
+client = udp_client.SimpleUDPClient("10.0.0.10",8888)
+main=tk.Tk()
+text = tk.StringVar()
+text.set("(^_^)")
+label = tk.Label(main, textvariable=text, font=("Helvetica", 50)).pack()
 
-def func(e=None):
-    client.send_message("/pi0", e.char)
+def funcAdd(e=None):
+    global outStr
+    outStr += e.char
+
+def funcClear(e=None):
+    global outStr
+    outStr = ""
+
+def funcSend(e=None):
+    text.set(outStr)
+    print(outStr)
+    syslog.syslog(outStr)
+    client.send_message("/pi0", outStr)
 
 def funcExit(e=None):
     sys.exit()
-    
-client = udp_client.SimpleUDPClient("10.0.0.10",8888)
-main=tk.Tk()
-main.bind('<KeyPress>',func)
+
+main.bind('a',funcClear)
+main.bind('b',funcSend)
+main.bind('0',funcAdd)
+main.bind('1',funcAdd)
+main.bind('2',funcAdd)
+main.bind('4',funcAdd)
 main.bind('<Escape>',funcExit)
 main.attributes("-fullscreen", True)
 main.configure(background='black')
